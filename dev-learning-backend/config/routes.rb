5d58926867b2296d5c7126e078e90ab6d583b505
arieldavis22 <aadavisbusiness@gmail.com>
2020-05-18 17:37:36 -0400

    teacher follow

--- dev-learning-backend/config/routes.rb
+++ dev-learning-backend/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "/autologin", to: "auth#autologin"
 
   post "/all-classrooms", to: "classrooms#all_classrooms"
-  post "new-classroom", to: "classrooms#new_classroom"
+  post "/new-classroom", to: "classrooms#new_classroom"
 
   get '/all-students', to: "users#all_students"
   get '/all-teachers', to: "users#all_teachers"
@@ -14,10 +14,10 @@ Rails.application.routes.draw do
   post '/follow', to: "users#teacher_follow"
 
   post '/add-student', to: "student_classrooms#add_student_to_class"
-  post 'find-students', to: "student_classrooms#find_students_in_class"
-  post 'find-classrooms', to: "student_classrooms#find_student_classrooms"
+  post '/find-students', to: "student_classrooms#find_students_in_class"
+  post '/find-classrooms', to: "student_classrooms#find_student_classrooms"
 
-  post "/newclass", to: "classrooms#new_class"
+  # post "/newclass", to: "classrooms#new_class"
 
   post "/newlesson", to: "lessons#create_lesson"
   post "/all-lessons", to: "lessons#all_lessons"
